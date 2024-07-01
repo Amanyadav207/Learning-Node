29b34f9ca056b37617185baac7c554ed809bc50a
Amanyadav207 <aman.23bcs10055@ms.sst.scaler.com>
2024-07-01 11:41:44 +0530

    login setup

--- index.js
+++ index.js
@@ -67,15 +67,37 @@ const http = require("http");
 
 const server = http.createServer((req,res)=>{
     res.setHeader("Content-Type","text/html");
-
+    if (req.url === "/login") {
+        res.write("<html><head><title>Node.js Class</title></head><body>");
+        res.write("<h1>Login Page</h1>");
+        res.write("</body></html>");
+        res.end();
+    }
+    else{
     res.write("<html><head><title>node js class</title> </head><body>");
     res.write("<h1>hello world</h1>");
     res.write("</body></html>");
     res.end();
-    
+    }
 });
 const port =3000;
 const hostname = "localhost";
+
+
+// on giving request /login it will show login page
+const url = require("url");
+
+// server.on("request", (req, res) => {
+//     if (req.url === "/login") {
+//         res.write("<html><head><title>Node.js Class</title></head><body>");
+//         res.write("<h1>Login Page</h1>");
+//         res.write("</body></html>");
+//         res.end();
+//     }
+// });
+// on giving request /register it will show register page
+
+
 server.listen(port,hostname,()=>{
     console.log("server is running on port 3000");
 });
